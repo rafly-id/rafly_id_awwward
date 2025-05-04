f7877ebb36a25b871f9876b7c79a7a6df18a6e68
rafly <raflym@gmail.com>
2025-05-04 17:52:05 +0700

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-5xl text-orange-500">Welcome to the React App</h1>
+      <h1 className="text-5xl text-violet-300 font-bold">Welcome to the React App</h1>
     </div>
   );
-}
+};
 
 export default App;
