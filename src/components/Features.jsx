a1da6db1436f2bbc1d025f2b47114b956ce9cd28
rafly <raflym@gmail.com>
2025-06-23 23:12:55 +0700

    make features file and first setup

---
+++ src/components/Features.jsx
@@ -0,0 +1,20 @@
+const Features = () => {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="font-circular-web text-lg text-blue-50">
+            Into the Metagame Layer
+          </p>
+          <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products converge into an interconnected overlay
+            experience on your world.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
