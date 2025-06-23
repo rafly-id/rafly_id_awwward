44a4d4eb7f5a6fae8dc3b318da1c543f59b26f6b
rafly <raflym@gmail.com>
2025-06-23 23:34:26 +0700

    make BentoCard in Feature and try it

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,24 @@
+const BentoCard = ({ src, title, description }) => {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 const Features = () => {
   return (
     <section className="bg-black pb-52">
@@ -11,6 +32,17 @@ const Features = () => {
             vibrant array of products converge into an interconnected overlay
             experience on your world.
           </p>
+        </div>
+        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <BentoCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radi<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across web2 and web3 games into a rewarding adventure."
+          />
         </div>
       </div>
     </section>
