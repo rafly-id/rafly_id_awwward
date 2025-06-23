8873ce45a6ddeca489acb9a12b1c1e1072e8c8ab
Rafly <muhr0417@gmail.com>
2025-06-23 22:57:10 +0700

    make adjustments in navbar and fix a few mistakes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
       className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass} `}
     >
       {leftIcon}
-      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase"></span>
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase"></span>
       <div>{title}</div>
       {rightIcon}
     </button>
